7290b7b7ac83c033104aa462fea4ed34fffa24ba
Eloi Alonso Quiñonero <97552454+Develoi89@users.noreply.github.com>
2023-10-31 17:24:05 +0100

    ex02 almost done

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -1,26 +1,56 @@
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 #include <stack>
-#include <vector>
 
 template<typename T>
 class MutantStack: public std::stack<T>
 {
-    private:
-        template<typename T>
-        vector<T> cp;
     public:
-        template<typename T>
-        MutantStack(){}
-        template<typename T>
-        MutantStack(Mutantstack const & c)
+        using typename std::stack<T>::container_type;
+        // using typename container_type::iterator;
+        class iterator
         {
-            
+            private:
+                typename container_type::iterator current;
+            public:
+                iterator(typename container_type::iterator current): current(current){}
+                iterator(iterator const &c): current(c.current){}
+                ~iterator(){}
+                iterator & operator++()
+                {
+                    ++current;
+                    return *this;
+                }
+                iterator & operator--()
+                {
+                    --current;
+                    return *this;
+                }
+                bool operator==(iterator const &m)
+                {
+                    return (current==m.current);
+                }
+                bool operator!=(iterator const &m)
+                {
+                    return !(current==m.current);
+                }
+                T& operator*()
+                {
+                    return *current;
+                }
+        };
+        MutantStack<T>(){}
+        MutantStack<T>(MutantStack const & c){}
+        MutantStack const & operator = (MutantStack const & c){}
+        ~MutantStack<T>(){}
+        iterator begin()
+        {
+            return iterator(std::stack<T>::c.begin());
         }
-        MutantStack const & operator = (MutantStack const & c);
-        ~MutantStack();
-        int begin();
-        int end();
+        iterator end()
+        {
+            return iterator(std::stack<T>::c.end());
+        }
 };
 
 #endif
